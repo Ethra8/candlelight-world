ee622eb34f851fed9faec3ede05ed7e26d85285e
Edna TMunill <ethra822@gmail.com>
2024-06-10 21:29:17 +0000

    room display, update booking times in Room model

--- bookings/models.py
+++ bookings/models.py
@@ -3,10 +3,8 @@ from django.contrib.auth import get_user_model
 
 
 TIME_SLOTS = [
-    ('08:00', '08:00 - 12:00 AM'),
-    ('12:00', '12:00 - 04:00 PM'),
-    ('16:00', '04:00 - 08:00 PM'),
-    ('20:00', '08:00 PM - 12:00 AM'),
+    ('10:00', '10:00 AM - 04:00 PM'),
+    ('18:00', '06:00 PM - 08:00 AM'),
 ]
 
 class Booking(models.Model):
@@ -14,6 +12,10 @@ class Booking(models.Model):
     date = models.DateField()
     start_time = models.CharField(max_length=5, choices=TIME_SLOTS)
     room = models.ForeignKey('worlds.Room', on_delete=models.CASCADE)
+    
+
+    def get_absolute_url(self):
+        return f"/bookings/{self.id}/"
 
     def __str__(self):
         return f'Booking {self.id} - {self.user.username} - {self.room.display_name} - {self.date} - {self.get_start_time_display()}'
--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import CreateBookingView
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path('bookings/new/', CreateBookingView.as_view(), name='booking_new'),
-    path('bookings/create, ')
+    # path('bookings/confirm, ')
+    path('bookings/<id:id>/', CreateBookingView.as_view(), name='worlds.index'),
 ]
